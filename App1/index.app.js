92c20a98a0a240fa617340bf8fff8763f90a1956
DESKTOP-51D5UO1\Banh Beo <hoaithuong23101997@gmail.com>
2018-11-14 15:34:11 +0700

    ! update App2 location

--- App1/index.app.js
+++ App1/index.app.js
@@ -1,50 +1,69 @@
 
-$("#btnSubmit").click(function() {
-    var name = $('#name').val();
-    var phone = $('#phone').val();
-    var address = $('#address').val();
-    var note = $('#note').val();
+// $("#btnSubmit").click(function() {
+//     var name = $('#name').val();
+//     var phone = $('#phone').val();
+//     var address = $('#address').val();
+//     var note = $('#note').val();
 
-    axios.post('http://localhost:3000/app1/request', {
-        name: name,
-        phone: phone,
-        address:address,
-        note:note,
-    })
-        .then(function (response) {
-            alert("thành công");
-        })
-        .catch(function (error) {
-            alert("lỗi");
-        });
+//     axios.post('http://localhost:3000/app1/request', {
+//         name: name,
+//         phone: phone,
+//         address:address,
+//         note:note,
+//     })
+//         .then(function (response) {
+//             alert("thành công");
+//         })
+//         .catch(function (error) {
+//             alert("lỗi");
+//         });
+    
+// });
+var vm = new Vue({
+    el: '#container',
+    data: {
+        userName: '',
+        password:'',
+        loginVisible:true,
+        requestsVisible:false,
+        mapVisible:false,
+        requests: [],
+        token:"",
+        refToken:"",
+        geocoder : {lat: 10.7623314, lng: 106.6820053},
+        address:"",
+        idEdit:0,
+        msg:"",
+        err:"",
 
-    // $.ajax({
-    //     url: 'http://localhost:3000/app1/request',
-    //     type: 'POST',
-    //     dataType: 'json',
-    //     timeout: 10000
-    // })
-    //     .fail(function () {
-    //         var source = document.getElementById("fail-template").innerHTML;
-    //         var template = Handlebars.compile(source);
-    //         var html = template();
-    //         var html2='<input type="text" class="form-control" >';
-    //         $('#vietnam').html(html2);
-    //         $('#fail').show();
-    //         $('#fail').html(html);
-    // })
-    //     .done(function(data) {
-    //     $.ajax({
-    //         url: 'http://localhost:3000/translate/EnglishToVietnamese?english='+data.english,
-    //         type: 'GET',
-    //         dataType: 'json',
-    //         timeout: 10000
-    //     }).done(function(data2) {
-    //         var source = document.getElementById("vietnamese-template").innerHTML;
-    //         var template = Handlebars.compile(source);
-    //         var html = template(data2);
-    //          $('#vietnam').html(html);
-    //          $('#fail').hide();
-    //     })
-    // })
-});
+        numDeltas : 100,
+        delay : 10,
+        i : 0,
+        deltaLat:0,
+        deltaLng:0
+    },
+    methods: {
+        login: function() {
+            var self = this;
+            self.err="";
+            axios.post('http://localhost:3000/app1/login', {
+                userName: self.userName,
+                password: self.password,
+            })
+                .then(function (response) {
+                    self.requestsVisible=true;
+                    self.loginVisible=false;
+                    self.token=response.data.access_token;
+                    self.refToken=response.data.refresh_token;
+                    self.getAllRequest();
+                })
+                .catch(function (error) {
+                    if (error.response.status===403){
+                        self.err="Bạn không có quyền sử dụng chức năng này"
+                    }else if (error.response.status===401) {
+                        self.err="Sai tên đăng nhập hoặc mật khẩu"
+                    }
+                })
+        },
+    }        
+})
